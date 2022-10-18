ce882007a3367e6c5d67a77c24fdd87376e587de
henryarias78 <115660608+henryarias78@users.noreply.github.com>
2022-10-18 09:57:09 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,12 +4,12 @@ function chillHop() {
     return [
 
         {
-            name:"Got Radio The 60's",
+            name:"1000 HITS 80s",
             artist: "",
             cover: "https://png.pngtree.com/png-vector/20190726/ourlarge/pngtree-modern-square-cool-music-for-party-lights-png-image_1629696.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
-            audio: "http://pureplay.cdnstream1.com/6011_128.mp3",
+            audio: "https://makri.cdnstream.com/1898_128",
             active: true,
         },
         
